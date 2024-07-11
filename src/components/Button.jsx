f9b940836821ea41f28f2b54308644163503e0fd
Marcos Vieira <marcosvieira482@gmail.com>
2024-07-11 01:18:29 -0300

    feat: add font and update styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
     return (
-        <div className={`btn !bg-black-100 flex flex-row p-4 md:p-5 items-center justify-center !rounded-3xl relative w-full border border-black-500 border-1 cursor-pointer ${props.class}`} onClick={props.click}>
+        <div className={`btn hover:bg-black-900 bg-black-100 hover:border-white flex flex-row p-4 md:p-5 items-center justify-center rounded-3xl relative w-full border border-black-900 border-1 cursor-pointer ${props.class}`} onClick={props.click}>
             <a href={props.href} target={props.target}>
                 <div className="flex items-center justify-center">
                     <div className="absolute left-4 w-[32px] md:w-[38px]">
